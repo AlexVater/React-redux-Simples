04bad68542e710dbef56151961e31f7d8392f05d
AlexVater <alexsander.vater@gmail.com>
2021-08-10 16:24:18 -0300

    Sem Redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
-import Card from "./components/Card";
+import Intervalo from "./components/Intervalo";
+import Media from "./components/Media";
+import Soma from "./components/Soma";
+import Sorteio from "./components/Sorteio";
+import {useState} from "react";
 
 function App() {
+    const [min, setMin] = useState(10)
+    const [max, setMax] = useState(1000)
+
     return (
         <div className="App">
             <h1>Exercício React-Redux (Simples)</h1>
             <div className="row">
-                <Card title="Card 1">X</Card>
+                <Intervalo min={min} max={max}
+                           onMinChange={setMin} onMaxChange={setMax} />
             </div>
             <div className="row">
-                <Card title="Card 2">Y</Card>
+                <Media min={min} max={max}/>
+                <Soma min={min} max={max}/>
+                <Sorteio min={min} max={max}/>
             </div>
         </div>
     );
